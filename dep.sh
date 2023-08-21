d25e82e073e938bb8af79648104a6dd93e7ba799
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-08-21 14:54:53 -0500

    1st

---
+++ dep.sh
@@ -0,0 +1,23 @@
+# Convert this to Python!
+
+src_dir(){
+mkdir -p ~/src
+}
+
+clone(){
+git clone git@github.com:Unicorn-OS/KVM.git
+git clone git@github.com:Unicorn-OS/Packer.git
+git clone git@github.com:Unicorn-OS/Packer.Template.git
+git clone git@github.com:Unicorn-OS/Vagrant.git
+git clone git@github.com:Source-Graph/VisualStudioCode.git
+}
+
+playbook(dir){
+cd $dir/src/ansible
+ansible-playbook uni.yml
+cd -
+}
+
+main(){
+cd ~/src
+}
