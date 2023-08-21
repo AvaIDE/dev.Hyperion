e24838df61ea3749b1c33dae20094e77eeb1c87e
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-08-21 15:11:40 -0500

    1st

--- dep.sh
+++ dep.sh
@@ -5,6 +5,8 @@ mkdir -p ~/src
 }
 
 clone(){
+git clone git@github.com:Source-Graph/PyCharm.Community.git
+git clone git@github.com:UniStor/MinIO.git
 git clone git@github.com:Unicorn-OS/KVM.git
 git clone git@github.com:Unicorn-OS/Packer.git
 git clone git@github.com:Unicorn-OS/Packer.Template.git
